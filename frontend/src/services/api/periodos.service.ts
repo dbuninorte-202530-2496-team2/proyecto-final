a435c075d3556874d60ca1e39ede7a59d6aa61df
Fatima <fatimac@uninorte.edu.co>
2025-12-02 14:01:29 -0500

    Funciona

--- frontend/src/services/api/periodos.service.ts
+++ frontend/src/services/api/periodos.service.ts
@@ -71,33 +71,3 @@ export const periodosService = {
         return response.data;
     },
 };
-// Periodos Service
-class PeriodosService {
-    private readonly BASE_PATH = '/periodos';
-
-    async getAll(): Promise<Periodo[]> {
-        const response = await apiClient.get<Periodo[]>(this.BASE_PATH);
-        return response.data;
-    }
-
-    async getById(id: number): Promise<Periodo> {
-        const response = await apiClient.get<Periodo>(`${this.BASE_PATH}/${id}`);
-        return response.data;
-    }
-
-    async create(data: CreatePeriodoDto): Promise<Periodo> {
-        const response = await apiClient.post<Periodo>(this.BASE_PATH, data);
-        return response.data;
-    }
-
-    async update(id: number, data: UpdatePeriodoDto): Promise<Periodo> {
-        const response = await apiClient.patch<Periodo>(`${this.BASE_PATH}/${id}`, data);
-        return response.data;
-    }
-
-    async delete(id: number): Promise<void> {
-        await apiClient.delete(`${this.BASE_PATH}/${id}`);
-    }
-}
-
-export const periodosService = new PeriodosService();
